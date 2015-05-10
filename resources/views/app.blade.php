dea8faead1bfa1ff7c3b95b7440bb6c811b28919
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2015-05-10 19:02:15 -0300

    change theme to material design

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,37 +7,25 @@
   <meta name="keywords" content="AngularJS, angular, bootstrap, admin, dashboard, panel, app, charts, bower_components,flat, responsive, layout, kit, ui, route, web, app, widgets" />
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 
-  <link rel="stylesheet" href="{{ asset('/bower_components/bootstrap/dist/css/bootstrap.css') }}" type="text/css" />
+  <!-- Vendor CSS -->
   <link rel="stylesheet" href="{{ asset('/bower_components/animate.css/animate.css') }}" type="text/css" />
-  <link rel="stylesheet" href="{{ asset('/bower_components/font-awesome/css/font-awesome.min.css') }}" type="text/css" />
-  <link rel="stylesheet" href="{{ asset('/bower_components/simple-line-icons/css/simple-line-icons.css') }}" type="text/css" />
-  <link rel="stylesheet" href="{{ asset('/css/font.css') }}" type="text/css') }}" />
+  <link href="{{ asset('/vendors/sweet-alert/sweet-alert.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('/vendors/material-icons/material-design-iconic-font.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('/vendors/socicon/socicon.min.css') }}" rel="stylesheet">
+
+  <!-- CSS -->
   <link rel="stylesheet" href="{{ asset('/css/app.css') }}" type="text/css') }}" />
 
 </head>
-<body>
-<div class="app app-header-fixed  ">
+<body class="login-content">
 
   @yield('content')
 
-  <!-- footer -->
-  <footer id="footer" class="app-footer" role="footer">
-        <div class="wrapper b-t bg-light">
-      <span class="pull-right">2.0.1 <a href ui-scroll="app" class="m-l-sm text-muted"><i class="fa fa-long-arrow-up"></i></a></span>
-    	&copy; {{date('Y')}} Copyright.
-    </div>
-  </footer>
-  <!-- / footer -->
-
-</div>
-
-<script src="{{ asset('/bower_components/jquery/dist/jquery.min.js') }}"></script>
-<script src="{{ asset('/bower_components/bootstrap/dist/js/bootstrap.js') }}"></script>
-<script src="{{ asset('/js/ui-load.js') }}"></script>
-<script src="{{ asset('/js/ui-jp.config.js') }}"></script>
-<script src="{{ asset('/js/ui-jp.js') }}"></script>
-<script src="{{ asset('/js/ui-nav.js') }}"></script>
-<script src="{{ asset('/js/ui-toggle.js') }}"></script>
+  <!-- Javascript -->
+  <script src="{{ asset('/bower_components/jquery/dist/jquery.min.js') }}"></script>
+  <script src="{{ asset('/bower_components/bootstrap/dist/js/bootstrap.js') }}"></script>
+  <script src="{{ asset('/vendors/waves/waves.min.js') }}"></script>
+  <script src="{{ asset('/js/functions.js') }}"></script>
 
 </body>
 </html>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,75 @@
 @extends('app')
 
 @section('content')
-<div class="container w-xxl w-auto-xs" ng-controller="SigninFormController" ng-init="app.settings.container = false;">
+
+  <!-- Login -->
+  <div class="lc-block toggled" id="l-login">
+    {!! Form::open(array('name' => 'form', 'class' => 'form-validation form-horizontal', 'role' => 'form')) !!}
+
+      <div class="text-danger wrapper text-center">
+				@if (count($errors) > 0)
+					<div class="alert alert-danger">
+						<strong>Whoops!</strong> There were some problems with your input.<br><br>
+						<ul>
+							@foreach ($errors->all() as $error)
+								<li>{{ $error }}</li>
+							@endforeach
+						</ul>
+					</div>
+				@endif
+      </div>
+
+      <div class="input-group m-b-20">
+        <span class="input-group-addon"><i class="md md-person"></i></span>
+          <div class="fg-line">
+            <input type="email" placeholder="Email" class="form-control no-border" name="email" value="{{ old('email') }}" >
+          </div>
+      </div>
+
+      <div class="input-group m-b-20">
+        <span class="input-group-addon"><i class="md md-accessibility"></i></span>
+        <div class="fg-line">
+          <input type="password" placeholder="Password" class="form-control no-border" name="password" >
+        </div>
+      </div>
+
+      <div class="clearfix"></div>
+
+      <div class="checkbox">
+        <label>
+          <input type="checkbox" value="">
+          <i class="input-helper"></i>
+          Keep me signed in
+        </label>
+      </div>
+
+      <a href="" class="btn btn-login btn-danger btn-float"><i class="md md-arrow-forward"></i></a>
+
+      <ul class="login-navigation">
+        <li data-block="#l-forget-password" class="bgm-orange">Forgot Password?</li>
+      </ul>
+    </div>
+  </form>
+
+  <!-- Forgot Password -->
+  <div class="lc-block" id="l-forget-password">
+      <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu risus. Curabitur commodo lorem fringilla enim feugiat commodo sed ac lacus.</p>
+
+      <div class="input-group m-b-20">
+          <span class="input-group-addon"><i class="md md-email"></i></span>
+          <div class="fg-line">
+              <input type="text" class="form-control" placeholder="Email Address">
+          </div>
+      </div>
+
+      <a href="" class="btn btn-login btn-danger btn-float"><i class="md md-arrow-forward"></i></a>
+
+      <ul class="login-navigation">
+          <li data-block="#l-login" class="bgm-green">Login</li>
+      </ul>
+  </div>
+
+<!-- <div class="container w-xxl w-auto-xs" ng-controller="SigninFormController" ng-init="app.settings.container = false;">
   <a href class="navbar-brand block m-t">Angulr</a>
   <div class="m-b-lg">
     <div class="wrapper text-center">
@@ -33,5 +101,6 @@
       <div class="line line-dashed"></div>
     </form>
   </div>
-</div>
+</div> -->
+
 @endsection
