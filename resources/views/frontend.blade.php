1957573d0135afdc1b09c357205ad790276cb2ee
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2015-05-11 13:18:02 -0300

    update frontend

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -2,10 +2,17 @@
 @section('body-class', 'class="login-content"')
 
 @section('content')
-  <div class="lc-block toggled text-left" style="width: 999px; margin-bottom: 30px;">
-    <img src="/img/comoaguilas.png" alt="">
-    <h3>Sitio en construccion</h3>
-    <p class="lead">Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</p>
+
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+      <div class="lc-block toggled text-left" >
+        <img class="img-responsive center-block" src="/img/comoaguilas.png" alt="">
+        <h3>Sitio en construccion</h3>
+        <p class="lead">Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus.</p>
+      </div>
+    </div>
   </div>
+</div>
 
 @endsection
